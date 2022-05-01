0df60281f4d76821ba96cf98afb9bc8e208b4e36
Pedro Cavalcante <pedrodanilo48@gmail.com>
2022-05-01 18:14:38 -0300

    aula nova

---
+++ js/botoes.js
@@ -0,0 +1,9 @@
+let btnPaginaInicial = document.querySelector('#PaginaInicial');
+let btnMetodo = document.querySelector('#Metodo');
+let btnAulas = document.querySelector('#Aulas');
+let btnRecomendacoes = document.querySelector('#Recomendacoes');
+
+btnPaginaInicial.addEventListener('click', () => btnPaginaInicial.style.backgroundColor = 'purple')
+btnMetodo.addEventListener('click', () => btnMetodo.style.backgroundColor = 'purple')
+btnAulas.addEventListener('click', () => btnAulas.style.backgroundColor = 'purple')
+btnRecomendacoes.addEventListener('click', () => btnRecomendacoes.style.backgroundColor = 'purple')
